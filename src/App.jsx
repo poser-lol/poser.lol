7ca7523ac216362aa83acf83524c42c763a66d2c
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2024-08-26 23:02:28 +0200

    Add supabase test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://pmjnfiqhiaxstnojglrq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtam5maXFoaWF4c3Rub2pnbHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3MDI0ODQsImV4cCI6MjA0MDI3ODQ4NH0.QpboF3v-ZEUpnAE7GEvdt6UxXQBGrOaLe1AQQMDHe6g");
 
 export function App() {
   const [milliseconds, setMilliseconds] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [intervalId, setIntervalId] = useState(undefined);
+  const [testData, setTestData] = useState([]);
+
+  useEffect(() => {
+    getTestData();
+  }, []);
+
+  async function getTestData() {
+    const { data } = await supabase.from("poser_test").select();
+    setTestData(data);
+  }
 
   useEffect(() => {
     if (isRunning) {
@@ -34,6 +47,12 @@ export function App() {
       <button onClick={stopTimer} disabled={!isRunning}>
         Stop
       </button>
+      <p>Test data:</p>
+      <ul>
+        {testData.map((data) => (
+          <li key={data.id}>{data.created_at}</li>
+        ))}
+      </ul>
     </div>
   );
 }
