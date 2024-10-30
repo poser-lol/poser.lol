f7ec2d9c8a63e3959167208b75d14db50f2ffe4e
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2024-10-30 21:45:51 +0900

    Simplify app more

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ export function App() {
 
   return (
     <div>
-      <h1>도경 안녕! 🛹</h1>
-      <h1>크리스 안녕 ^^ </h1>
-      <small>This is a React app 헤hehhhe</small>
       <p>Count: {count}</p>
       <button onClick={increaseCount}>Increase</button>
     </div>
