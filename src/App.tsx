eea0eeafbb4f9ee90234e3aba5dd05f89cf044f2
dodo <cads225@gachon.ac.kr>
2024-11-11 19:37:10 +0900

    error 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Spot } from "./Spot";
 import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "../styles/theme";
+import { ThemeProvider } from "styled-components";
 
 export function App() {
   return (
     <>
-      <CssBaseline />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/spot/:id" element={<Spot />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/spot/:id" element={<Spot />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
