06470fab0c3598f1df3cf2a7489c6e36e4618944
dodo <cads225@gachon.ac.kr>
2024-11-11 22:02:41 +0900

    custom overlay

--- src/components/MapOverlay/MapOverlay.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as S from "./MapOverlay.styled";
-export default function MapOverlay() {
-  var content =
-    '<div><span class="left"></span><h1>카카오!</h1><span class="right"></span></div>';
-  return content;
-}
