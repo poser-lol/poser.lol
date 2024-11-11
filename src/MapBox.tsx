a0619a004ea33b07c8373c825bd3f39e29e17dab
dodo <cads225@gachon.ac.kr>
2024-11-11 22:10:41 +0900

    error fix

--- src/MapBox.tsx
+++ src/MapBox.tsx
@@ -14,56 +14,50 @@ export default function MapBox() {
   const DEFAULT_LNG = 127.03088379;
 
   useEffect(() => {
-    const loadKaKaoMap = () => {
-      if (window.kakao && !map) {
-        window.kakao.maps.load(() => {
-          const mapContainer = document.getElementById("map");
-          const mapOption = {
-            center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
-            level: 7,
-          };
-          const mapInstance = new window.kakao.maps.Map(
-            mapContainer,
-            mapOption,
+    if (window.kakao) {
+      const loadKaKaoMap = () => {
+        const mapContainer = document.getElementById("map");
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+          level: 7,
+        };
+        const mapInstance = new window.kakao.maps.Map(mapContainer, mapOption);
+        setMap(mapInstance);
+
+        spots?.forEach((info) => {
+          const position = new window.kakao.maps.LatLng(
+            info.latitude,
+            info.longitude
           );
-          setMap(mapInstance);
 
-          spots?.forEach((info) => {
-            const position = new window.kakao.maps.LatLng(
-              info.latitude,
-              info.longitude,
-            );
-
-            const content = `
-              <div style="width: 70px; height: 70px; text-align: center;">
-                <div 
-                  style="width: 100%; height: 100%; background-image: url(${wheel}); background-size: contain; background-repeat: no-repeat; background-position: center;">
-                </div>
-                
+          const content = `
+            <div style="width: 70px; height: 70px; text-align: center;">
+              <div 
+                style="width: 100%; height: 100%; background-image: url(${wheel}); background-size: contain; background-repeat: no-repeat; background-position: center;">
               </div>
-            `;
+            </div>
+          `;
 
-            const customOverlay = new window.kakao.maps.CustomOverlay({
-              position: position,
-              content: content,
-            });
-
-            customOverlay.setMap(mapInstance);
+          const customOverlay = new window.kakao.maps.CustomOverlay({
+            position: position,
+            content: content,
           });
+
+          customOverlay.setMap(mapInstance);
         });
-      }
-    };
+      };
 
-    const script = document.createElement("script");
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_JS_KEY}&libraries=services&autoload=false`;
-    script.async = true;
-    script.onload = loadKaKaoMap;
-    document.head.appendChild(script);
+      const script = document.createElement("script");
+      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_JS_KEY}&libraries=services&autoload=false`;
+      script.async = true;
+      script.onload = loadKaKaoMap;
+      document.head.appendChild(script);
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, [map]);
+      return () => {
+        document.head.removeChild(script);
+      };
+    }
+  }, []);
 
   return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
 }
